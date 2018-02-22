29e380fb1b2b5b1e3c9f66106ac6b337a117312a
Michael Berend <michaelberend@michaels-macbook-air.local>
2018-02-22 16:24:01 +0100

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Michael Berend on 22/02/2018.
+//  Copyright © 2018 Michael Berend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
